7426f44acbee01756dda60d64bae9742c8adbf16
Aleksey Ivanov <ialexxei@gmail.com>
2018-04-25 09:15:42 +0300

    Return FutureResult in Ping RPC

--- src/rpc/ping.rs
+++ src/rpc/ping.rs
@@ -1,16 +1,18 @@
-use rpc::error::Result;
+use futures::future::{self, FutureResult};
+
+use rpc::error::Error;
 
 build_rpc_trait! {
     pub trait Rpc {
         #[rpc(name = "ping")]
-        fn ping(&self) -> Result<String>;
+        fn ping(&self) -> FutureResult<String, Error>;
     }
 }
 
 pub struct RpcImpl;
 
 impl Rpc for RpcImpl {
-    fn ping(&self) -> Result<String> {
-        Ok("pong".to_owned())
+    fn ping(&self) -> FutureResult<String, Error> {
+        future::ok("pong".to_owned())
     }
 }
