ad1c598140e62c6e3deef1a690e78943325c014d
Aleksey Ivanov <ialexxei@gmail.com>
2018-05-28 16:15:46 +0300

    Authorize Object API

--- tests/api/shared/mod.rs
+++ tests/api/shared/mod.rs
@@ -1,6 +1,8 @@
 use actix_web::{self, http, test::TestServer};
 use diesel;
+use diesel::prelude::*;
 use iam;
+use uuid::Uuid;
 
 pub struct Server {
     pub srv: TestServer,
@@ -53,4 +55,57 @@ fn init() {
 
 pub fn strip_json(json: &str) -> String {
     json.replace('\n', "").replace(' ', "")
+}
+
+pub fn grant_namespace_ownership(conn: &PgConnection, namespace_id: Uuid, account_id: Uuid) {
+    use iam::models::*;
+    use iam::schema::*;
+
+    diesel::insert_into(abac_subject_attr::table)
+        .values(NewAbacSubjectAttr {
+            namespace_id: namespace_id,
+            subject_id: account_id,
+            key: "owner:namespace".to_owned(),
+            value: namespace_id.to_string(),
+        })
+        .execute(conn)
+        .unwrap();
+
+    diesel::insert_into(abac_object_attr::table)
+        .values(NewAbacObjectAttr {
+            namespace_id: namespace_id,
+            object_id: format!("namespace.{}", namespace_id),
+            key: "belongs_to:namespace".to_owned(),
+            value: namespace_id.to_string(),
+        })
+        .execute(conn)
+        .unwrap();
+
+    diesel::insert_into(abac_action_attr::table)
+        .values(NewAbacActionAttr {
+            namespace_id: namespace_id,
+            action_id: "execute".to_owned(),
+            key: "access".to_owned(),
+            value: "*".to_owned(),
+        })
+        .execute(conn)
+        .unwrap();
+
+    diesel::insert_into(abac_policy::table)
+        .values(NewAbacPolicy {
+            namespace_id: namespace_id,
+            subject_namespace_id: namespace_id,
+            subject_key: "owner:namespace".to_owned(),
+            subject_value: namespace_id.to_string(),
+            object_namespace_id: namespace_id,
+            object_key: "belongs_to:namespace".to_owned(),
+            object_value: namespace_id.to_string(),
+            action_namespace_id: namespace_id,
+            action_key: "access".to_owned(),
+            action_value: "*".to_owned(),
+            not_before: None,
+            expired_at: None,
+        })
+        .execute(conn)
+        .unwrap();
 }
