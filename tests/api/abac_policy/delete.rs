07c508ac9b56a6b09ccd49db1ab822737c52ea4e
Aleksey Ivanov <ialexxei@gmail.com>
2018-07-13 18:37:37 +0300

    Update abac_policy authz rules

--- tests/api/abac_policy/delete.rs
+++ tests/api/abac_policy/delete.rs
@@ -2,10 +2,11 @@ use diesel::{self, prelude::*};
 use serde_json;
 use uuid::Uuid;
 
-use abac::models::{AbacObject, AbacPolicy};
-use abac::schema::{abac_object, abac_policy};
+use abac::models::AbacPolicy;
+use abac::schema::abac_policy;
 use abac::types::AbacAttribute;
 
+use iam::abac_attribute::{CollectionKind, OperationKind, UriKind};
 use iam::models::{Account, Namespace};
 
 use shared::db::{create_account, create_namespace, create_operations, AccountKind, NamespaceKind};
@@ -67,25 +68,6 @@ fn before_each_1(conn: &PgConnection) -> ((Account, Namespace), (Account, Namesp
     let foxford_account = create_account(conn, AccountKind::Foxford);
     let foxford_namespace = create_namespace(conn, NamespaceKind::Foxford(foxford_account.id));
 
-    let netology_account = create_account(conn, AccountKind::Netology);
-    let _netology_namespace = create_namespace(conn, NamespaceKind::Netology(netology_account.id));
-
-    diesel::insert_into(abac_object::table)
-        .values(AbacObject {
-            inbound: AbacAttribute {
-                namespace_id: foxford_namespace.id,
-                key: "type".to_owned(),
-                value: "abac_policy".to_owned(),
-            },
-            outbound: AbacAttribute {
-                namespace_id: iam_namespace.id,
-                key: "uri".to_owned(),
-                value: format!("namespace/{}", foxford_namespace.id),
-            },
-        })
-        .execute(conn)
-        .unwrap();
-
     (
         (iam_account, iam_namespace),
         (foxford_account, foxford_namespace),
@@ -130,12 +112,13 @@ mod with_existing_record {
         }
 
         #[test]
-        fn can_delete_alien_record() {
+        fn cannot_delete_alien_record() {
             let shared::Server { mut srv, pool } = shared::build_server();
 
             {
                 let conn = get_conn!(pool);
                 let _ = before_each_2(&conn);
+                let _ = create_account(&conn, AccountKind::Netology);
             }
 
             let req = shared::build_auth_request(
@@ -150,6 +133,51 @@ mod with_existing_record {
             {
                 let conn = get_conn!(pool);
                 assert_eq!(find_record(&conn), Ok(1));
+            }
+        }
+
+        #[test]
+        fn can_delete_alien_record_when_permission_granted() {
+            let shared::Server { mut srv, pool } = shared::build_server();
+
+            {
+                let conn = get_conn!(pool);
+                let _ = before_each_2(&conn);
+
+                let netology_account = create_account(&conn, AccountKind::Netology);
+
+                diesel::insert_into(abac_policy::table)
+                    .values(AbacPolicy {
+                        subject: vec![AbacAttribute::new(
+                            *IAM_NAMESPACE_ID,
+                            UriKind::Account(netology_account.id),
+                        )],
+                        object: vec![
+                            AbacAttribute::new(
+                                *IAM_NAMESPACE_ID,
+                                UriKind::Namespace(*FOXFORD_NAMESPACE_ID),
+                            ),
+                            AbacAttribute::new(*IAM_NAMESPACE_ID, CollectionKind::AbacPolicy),
+                        ],
+                        action: vec![AbacAttribute::new(*IAM_NAMESPACE_ID, OperationKind::Delete)],
+                        namespace_id: *IAM_NAMESPACE_ID,
+                    })
+                    .execute(&conn)
+                    .unwrap();
+            }
+
+            let req = shared::build_auth_request(
+                &srv,
+                serde_json::to_string(&build_request()).unwrap(),
+                Some(*NETOLOGY_ACCOUNT_ID),
+            );
+            let resp = srv.execute(req.send()).unwrap();
+            let body = srv.execute(resp.body()).unwrap();
+            assert_eq!(body, *EXPECTED);
+
+            {
+                let conn = get_conn!(pool);
+                assert_eq!(find_record(&conn), Ok(0));
             }
         }
     }
