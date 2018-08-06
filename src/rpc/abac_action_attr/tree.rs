326ac602019e8b6fa45e68e81d0a98ad99af7f79
Aleksey Ivanov <ialexxei@gmail.com>
2018-08-06 19:56:03 +0300

    Implement Action Tree API

---
+++ src/rpc/abac_action_attr/tree.rs
@@ -0,0 +1,47 @@
+use futures::{future, Future};
+
+use rpc;
+
+pub type Request = rpc::TreeRequest;
+pub type Response = rpc::TreeResponse;
+
+pub fn call(meta: rpc::Meta, req: Request) -> impl Future<Item = Response, Error = rpc::Error> {
+    use abac_attribute::{CollectionKind, OperationKind};
+    use actors::db::tree;
+    use rpc::authorize_collection;
+
+    future::result(rpc::forbid_anonymous(meta.subject))
+        .and_then({
+            let db = meta.db.clone().unwrap();
+            let ns_id = req.filter.attribute.namespace_id;
+
+            move |subject_id| {
+                let collection = CollectionKind::AbacAction;
+                let operation = OperationKind::List;
+
+                authorize_collection(&db, ns_id, subject_id, collection, operation)
+            }
+        })
+        .and_then({
+            let limit = req.pagination.limit;
+            move |_| rpc::pagination::check_limit(limit)
+        })
+        .and_then({
+            let db = meta.db.unwrap();
+            move |_| {
+                let msg = tree::Tree(
+                    tree::CollectionKind::AbacAction,
+                    tree::Select {
+                        direction: req.filter.direction,
+                        attribute: req.filter.attribute,
+                        limit: req.pagination.limit,
+                        offset: req.pagination.offset,
+                    },
+                );
+                db.send(msg).from_err().and_then(|res| {
+                    debug!("abac action select res: {:?}", res);
+                    Ok(Response::from(res?))
+                })
+            }
+        })
+}
