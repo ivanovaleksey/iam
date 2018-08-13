e726bcebe48525e04359a29a4ff522a515d41c8b
Aleksey Ivanov <ialexxei@gmail.com>
2018-08-13 14:55:25 +0300

    Move ca.crt into scripts/

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -12,7 +12,7 @@ export PATH=${HOME}/.local/bin:${PATH}
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl ${HOME}/.local/bin
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && chmod +x skaffold && mv skaffold ${HOME}/.local/bin
 
-kubectl config set-cluster media --embed-certs --server ${KUBE_SERVER} --certificate-authority k8s/ca.crt
+kubectl config set-cluster media --embed-certs --server ${KUBE_SERVER} --certificate-authority ca.crt
 kubectl config set-credentials travis --token ${KUBE_TOKEN}
 kubectl config set-context media --cluster media --user travis --namespace=${NAMESPACE}
 kubectl config use-context media
